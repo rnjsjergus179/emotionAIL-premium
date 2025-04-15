46f326a600935ff80d270ab978137beeea64e7e0
rnjsjergus179 <rnjsjergus179@gmail.com>
2025-04-15 16:01:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('EmotionAIL 백엔드 실행 중');
+});
+
+// 여기서 외부 IP를 확인하는 라우트
+app.get('/myip', async (req, res) => {
+  try {
+    const response = await axios.get('https://api64.ipify.org?format=json');
+    res.send(`현재 서버 IP: ${response.data.ip}`);
+  } catch (error) {
+    res.status(500).send('IP 확인 중 오류 발생');
+  }
 });
 
 const PORT = process.env.PORT || 3000;
